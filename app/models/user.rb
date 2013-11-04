aa87169e51fdb5216e241196dab0065714af97f7
Mark Miyashita <negativetwelve@gmail.com>
2013-11-04 01:50:04 -0800

    updating project and user models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,7 @@
 class User < ActiveRecord::Base
   has_secure_password
-  has_many :microposts, dependent: :destroy
-  has_many :relationships, foreign_key: "follower_id", dependent: :destroy
-  has_many :followed_users, through: :relationships, source: :followed
-  has_many :reverse_relationships, foreign_key: "followed_id",
-                                   class_name:  "Relationship",
-                                   dependent:   :destroy
-  has_many :followers, through: :reverse_relationships, source: :follower
+
+  has_and_belongs_to_many :projects
 
   before_save { email.downcase! }
   before_create :create_remember_token
@@ -25,22 +20,6 @@ class User < ActiveRecord::Base
 
   def User.encrypt(token)
     Digest::SHA1.hexdigest(token.to_s)
-  end
-
-  def feed
-    Micropost.from_users_followed_by(self)
-  end
-
-  def following?(other_user)
-    relationships.find_by_followed_id(other_user.id)
-  end
-
-  def follow!(other_user)
-    relationships.create!(followed_id: other_user.id)
-  end
-
-  def unfollow!(other_user)
-    relationships.find_by_followed_id(other_user.id).destroy
   end
 
   private
