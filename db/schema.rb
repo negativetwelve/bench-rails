e87296f48cbfe61d274bedcf50a02564f260ae9e
Mark Miyashita <negativetwelve@gmail.com>
2013-11-04 01:49:51 -0800

    Adding projects and project users join table

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20120308193644) do
+ActiveRecord::Schema.define(version: 20131104094739) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
   enable_extension "hstore"
+
+  create_table "projects", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "projects_users", force: true do |t|
+    t.integer "project_id"
+    t.integer "user_id"
+  end
 
   create_table "users", force: true do |t|
     t.string   "name"
